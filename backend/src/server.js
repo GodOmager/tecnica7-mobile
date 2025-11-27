53827ecbeac66cadc02b9ded4c4f76d2b5cbdd68
WendyEducation <callizayazenteno.wendy@tecnica7.edu.ar>
2025-11-27 20:38:24 -0300

    servidor malo

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+import cors from "cors";
+import pkg from "@prisma/client";
+const { PrismaClient } = pkg;
+
+const app = express();
+const prisma = new PrismaClient();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Backend funcionando!"));
+
+app.listen(3000, () => console.log("Servidor corriendo en http://localhost:3000"));
